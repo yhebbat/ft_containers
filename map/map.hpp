2d542382a2f8981706583c507460e5cdd1c86cae
Yassine Hebbat <yhebbat@e1r8p10.1337.ma>
2022-07-28 22:50:43 +0100

    123

--- map/map.hpp
+++ map/map.hpp
@@ -44,7 +44,7 @@ namespace ft
         typedef typename allocator_type::const_reference                        const_reference;
         typedef typename allocator_type::pointer                                pointer;
         typedef typename allocator_type::const_pointer                          const_pointer;
-        typedef typename ft::bidirectional_iterator<value_type, key_compare>    iterator;
+        typedef typename ft::bidirectional_iterator<value_type, key_compare>            iterator;
         typedef typename ft::bidirectional_iterator< const value_type,const key_compare> const_iterator;
         typedef typename ft::reverse_iterator<iterator>                         reverse_iterator;
         typedef typename ft::reverse_iterator<const_iterator>                   const_reverse_iterator;
